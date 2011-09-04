5c6f261fb65a828e1ed64272fe9b6e817cebbe5e
cfddream <cfddream@gmail.com>
2011-09-05 00:36:39 +0800

    updae test file

--- test/aop.js
+++ test/aop.js
@@ -32,7 +32,7 @@ var appAround1 = new Advice({name: 'app around 1'}, function (advice, args){
 h.add(appAround1);
 
 var appAround2 = new Advice({name: 'app around 2'}, function (advice){
-    console.log(this.name);
+   console.log(this.name);
     //console.dir(advice);
     advice.exec();
     console.log(this.name);
@@ -46,17 +46,30 @@ h.after(function (){
     console.log(this.name)
 }, {name: 'app after 1'});
 
-
 h.after(function (){
     console.log('after...do some thing...')
     return new Halt('dd', 789);
+});
+
+/*
+var ba = new Advice({name: 'app before and after'}, function (advice){
+    console.log('before')
+    console.log(this.name);
+    console.log('after')
+}, 3);
+
+h.add(ba);
+*/
+
+h.inject(3, function() {
+    console.log('inject before and after.')
 });
 
 h.before(function (){
     console.log(this.name)
 }, {name: 'app before 1'});
 
-//console.dir(h);
+console.dir(h);
 
 console.log(h.exec(1,2,3));
 
